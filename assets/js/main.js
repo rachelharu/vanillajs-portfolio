9889547ba2f00b9f922b04ddb416a2fc14fc111a
rachelharu <rachel.haru9@gmail.com>
2022-09-24 13:28:33 -0700

    create project and skills section, create javascript code for tab switching

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,26 @@
 /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tc) => {
+      // tc = tabContents
+      tc.classList.remove("filters__active");
+    });
+    target.classList.add("filters__active");
+
+    tabs.forEach((t) => {
+      // t = tab
+      t.classList.remove("filter-tab-active");
+    });
+    tab.classList.add("filter-tab-active");
+  });
+});
 
 /*=============== DARK LIGHT THEME ===============*/
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-console.log('test');
+console.log("test");
