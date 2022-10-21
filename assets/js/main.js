d3d5b7ae4c89ec3bccd72ff703c5256cae49c15d
rachelharu <rachel.haru9@gmail.com>
2022-10-20 17:38:44 -0700

    edit scrollreveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,14 +61,14 @@ const sr = ScrollReveal({
   origin: "top",
   distance: "60px",
   duration: 2500,
-  delay: 400,
+  delay: 200,
 });
 
 sr.reveal(`.profile__border`);
-sr.reveal(`.profile__name`, { delay: 500 });
-sr.reveal(`.profile__profession`, { delay: 600 });
-sr.reveal(`.profile__social`, { delay: 700 });
-sr.reveal(`.profile__info-group`, {interval:100, delay: 700 });
-sr.reveal(`.profile__buttons`, { delay: 800 });
-sr.reveal(`.filters__content`, { delay: 900 });
-sr.reveal(`.filters`, { delay: 1000 })
+sr.reveal(`.profile__name`, { delay: 400 });
+sr.reveal(`.profile__profession`, { delay: 500 });
+sr.reveal(`.profile__social`, { delay: 600 });
+sr.reveal(`.profile__info-group`, {interval:100, delay: 600 });
+sr.reveal(`.profile__buttons`, { delay: 600 });
+sr.reveal(`.filters__content`, { delay: 600 });
+sr.reveal(`.filters`, { delay: 700 })
